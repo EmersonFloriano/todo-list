97141e78c2549ae95770922a656b3c1fa707c6f7
EmersonFloriano <florianoe574@gmail.com>
2022-07-14 12:43:48 -0300

    deploy3

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git config  user.email "florianoe574@gmail.com"
-git config  user.name "EmersonFloriano"
 
 git push -f git@github.com:EmersonFloriano/todo-list.git main:gh-pages
 
